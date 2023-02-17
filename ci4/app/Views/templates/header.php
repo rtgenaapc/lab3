18bc09be1c58f98bb909ab7142ad15639265e3bb
Reiner T. Gena <rtgena@student.apc.edu.ph>
2023-02-17 09:01:51 +0800

    Update Content

---
+++ ci4/app/Views/templates/header.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<head>
+    <?php echo link_tag('css/mystyles.css'); ?>
+    <title>CodeIgniter Tutorial - GENA</title>
+</head>
+<body>
+
+    <h1><?= esc($title) ?></h1>
---
+++ ci4/public/js/message.js
@@ -0,0 +1,7 @@
+const message = () => {
+    const name = "Bryan";
+    const age = 2;
+    return name + ' is ' + age + 'years old.';
+    };
+    
+    export default message;
