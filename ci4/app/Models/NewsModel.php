c410af69f86ec27622b6361425c33087b65a1dc0
Reiner T. Gena <119903810+rtgenaapc@users.noreply.github.com>
2023-02-27 08:53:54 +0800

    Update NewsModel.php

--- ci4/app/Models/NewsModel.php
+++ ci4/app/Models/NewsModel.php
@@ -6,7 +6,7 @@ use CodeIgniter\Model;
 
 class NewsModel extends Model
 {
-    protected $table = 'news';
+    protected $table = 'rtgena_news';
     protected $allowedFields = ['title', 'slug', 'body'];
 
     public function getNews($slug = false)
