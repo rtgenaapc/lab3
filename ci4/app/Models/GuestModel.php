007180c0ea92c27a7a85983704a71432b43492c0
Reiner T. Gena <rtgena@student.apc.edu.ph>
2023-02-28 14:42:14 +0800

    update GuestModel

--- ci4/app/Models/GuestModel.php
+++ ci4/app/Models/GuestModel.php
@@ -7,10 +7,9 @@ use CodeIgniter\Model;
 class GuestModel extends Model
 {
     protected $table = 'Gena_MyGuests';
+    
     public function getGuest()
     {
-        if ($slug === false) {
             return $this->findAll();
-        }
     }
 }
